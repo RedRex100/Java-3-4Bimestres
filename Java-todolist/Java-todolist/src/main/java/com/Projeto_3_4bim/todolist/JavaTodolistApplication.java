d0d45b5493a31d9adbe0eff18962864c62d8711a
KaioDamasceno <damasceno.kaio1@gmail.com>
2025-09-28 00:19:23 -0300

    carcaça
    definição de dependencias:
    criação da estrutura de pastas.

---
+++ Java-todolist/Java-todolist/src/main/java/com/Projeto_3_4bim/todolist/JavaTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.Projeto_3_4bim.Java_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTodolistApplication.class, args);
+	}
+
+}
---
+++ Java-todolist/Java-todolist/src/test/java/com/Projeto_3_4bim/todolist/JavaTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Projeto_3_4bim.Java_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
